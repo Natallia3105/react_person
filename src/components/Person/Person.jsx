9f4432f73443d318454c0e5a1f7642d2d9472441
Nataliia Honcharuk <133349158+Natallia3105@users.noreply.github.com>
2024-05-24 21:56:22 +0200

    Update src/components/Person/Person.jsx
    Co-authored-by: Nazar Matsevych <80700247+nazarmatsevych@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,12 +6,11 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
-      {isMarried === true && (
-        <p className="Person__partner">{`${partnerName} is my ${partner}`}</p>
-      )}
-      {isMarried === false && (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partner}`
+          : 'I am not married'}
+      </p>
     </section>
   );
 };
